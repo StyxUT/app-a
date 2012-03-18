40ad080e02f3025e3b3b679ba9b0fa923d9b3273
Eric Barnes <styxut@Erics-Mac-Pro.local>
2012-03-17 21:01:18 -0600

    created address model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120318022032) do
+ActiveRecord::Schema.define(:version => 20120318025731) do
+
+  create_table "addresses", :force => true do |t|
+    t.string   "address1"
+    t.string   "address2"
+    t.string   "city"
+    t.string   "state"
+    t.string   "zip"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "people", :force => true do |t|
     t.string   "first_name"
